4aab699b300481936ca2cc7cec50600cf9a41e20
Flávia Costa <flavia.cbrasileiro@gmail.com>
2023-07-04 16:35:46 -0300

    adicionar js

---
+++ intro-component-with-signup-form-master/index.js
@@ -0,0 +1,13 @@
+const inputForm = {
+    nome: "Flávia",
+    sobrenome: "Costa",
+    email: "flaviacosta@email.com",
+    senha: 12345,
+};
+
+function login() {
+    if (condition) {
+        
+    }
+}
+
