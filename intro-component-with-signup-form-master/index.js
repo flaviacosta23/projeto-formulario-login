0b2bdf5b1a6415103590a0d1d4f7126a9d6aa85f
Flávia Costa <flavia.cbrasileiro@gmail.com>
2023-07-05 17:48:26 -0300

    iniciando a função do login

--- intro-component-with-signup-form-master/index.js
+++ intro-component-with-signup-form-master/index.js
@@ -1,13 +1,10 @@
-const inputForm = {
-    nome: "Flávia",
-    sobrenome: "Costa",
-    email: "flaviacosta@email.com",
-    senha: 12345,
-};
-
 function login() {
-    if (condition) {
-        
+    const inputForm = "Flávia"
+    
+    const input = document.getElementById("input1");
+
+    if (input.value===inputForm) {
+        input.innerHTML = "Login";
     }
 }
 
