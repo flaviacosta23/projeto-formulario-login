fb9d60c4c19f8ea64c04f1112ae53d30b0a7ba96
Flávia Costa <flavia.cbrasileiro@gmail.com>
2023-07-13 16:44:45 -0300

    ultimas alterações finalizando projeto

--- intro-component-with-signup-form-master/index.js
+++ intro-component-with-signup-form-master/index.js
@@ -1,5 +1,5 @@
-function login() {
-    const inputForm = {
+function login() { //função para efetuar login
+    const inputForm = { //valores que farão o login acontecer, escolhidos para exemplo
         nome: "Flávia",
         sobrenome: "Costa",
         email: 'flavia@email.com',
@@ -13,11 +13,15 @@ function login() {
     
     const inputAll = document.querySelectorAll('input');
 
-    if (firstName == inputForm.nome && lastName == inputForm.sobrenome && email == inputForm.email && password == inputForm.senha) {
-        alert("Login");
+    if (firstName == inputForm.nome && lastName == inputForm.sobrenome && 
+        email == inputForm.email && password == inputForm.senha) { //Condição se os dados do login estiverem corretos
+        for (var i = 0; i < inputAll.length; i++) { //seleção de todos os inputs e deixando a borda verde
+            inputAll[i].style.borderColor = 'green';
+        }
+
     } else {
-        for (var i = 0; i < inputAll.length; i++) {
-            inputAll[i].style.borderColor = 'hsl(0, 100%, 74%)';
+        for (var i = 0; i < inputAll.length; i++) { //condição se os dados estiverem errados
+            inputAll[i].style.borderColor = 'hsl(0, 100%, 74%)';  
 
             inputAll[i].style.backgroundImage = "url('icone.svg')";
 
@@ -25,7 +29,7 @@ function login() {
 
             inputAll[i].style.backgroundPosition = "calc(100% - 15px) center";
 
-            if (i === 2) {
+            if (i === 2) { //condição especifica para o 3 input
                 inputAll[i].value = 'email@exemplo.com';
                 inputAll[i].style.color = 'hsl(0, 100%, 74%)';
             }
